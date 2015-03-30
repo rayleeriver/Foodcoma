0a7314ed9b69cdb3c0c7704230926840d3eee697
Vee Trairattanapa <happierv@gmail.com>
2015-03-29 23:20:14 -0700

    Clean up parcelable-related methods

--- app/src/main/java/com/swpbiz/foodcoma/models/Invitation.java
+++ app/src/main/java/com/swpbiz/foodcoma/models/Invitation.java
@@ -181,16 +181,15 @@ public class Invitation implements Parcelable {
         dest.writeString(placeName);
         dest.writeString(mapUrl);
         dest.writeLong(timeOfEvent);
-//        dest.writeMap(users);
+
+        // Write the size of users HashMap
         dest.writeInt(users.size());
+
+        // Write each key and value of users HashMap
         for(Map.Entry<String, User> item : users.entrySet()) {
             dest.writeString(item.getKey());
             dest.writeParcelable(item.getValue(), PARCELABLE_WRITE_RETURN_VALUE);
         }
-
-//        Bundle usersBundle = new Bundle();
-//        usersBundle.putSerializable("users", users);
-//        dest.writeBundle(usersBundle);
 
         dest.writeByte((byte) (accept ? 1 : 0));
     }
@@ -216,12 +215,16 @@ public class Invitation implements Parcelable {
         placeName = in.readString();
         mapUrl = in.readString();
         timeOfEvent = in.readLong();
-//        users = (HashMap<String, User>) in.readBundle().getSerializable("users");
-//        users = in.readHashMap(String.class.getClassLoader());
+
+        // Get the size of users HashMap
         int userSize = in.readInt();
+
+        // Create HashMap if needed
         if(userSize > 0){
             users = new HashMap<String, User>();
         }
+
+        // Populate data into the HashMap
         for(int i = 0; i < userSize; i++) {
             users.put(in.readString(), (User) in.readParcelable(User.class.getClassLoader()));
         }
